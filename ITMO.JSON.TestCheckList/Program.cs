052d80de869ba73268ca37ab8efb849bb76ea67a
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-09 09:43:00 +0300

    Create Делаем еще один проект  и переписываем все с самого начала

---
+++ ITMO.JSON.TestCheckList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITMO.JSON.TestCheckList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
