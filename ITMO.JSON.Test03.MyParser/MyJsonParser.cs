8541409c85aa0205d8eb7e409417156d1e4c53f7
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-15 20:14:56 +0300

    Добавлен класс Expando, позволяющий создать класс с динамическим колличеством полей на основе строк

---
+++ ITMO.JSON.Test03.MyParser/MyJsonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.JSON.MyParser
+{
+    class MyJsonParser
+    {    
+
+        MyJsonParser(string namefile)
+        {
+
+
+        }
+    }
+}
--- ITMO.JSON.Test03.MyParser/Program.cs
+++ ITMO.JSON.Test03.MyParser/Program.cs
@@ -12,69 +12,18 @@ namespace ITMO.JSON.MyParser
     {
         static void Main(string[] args)
         {
-            Dictionary<string, object> dictionary = new Dictionary<string, object>()
-                                                {
-                                                    {"Ali", "Ostad"}
-                                                };
 
-            dictionary.Ali
 
-            dynamic obj = new ImpromptuDictionary()
+            dynamic obj = new Expando()
             {
                 { "foo", "hello" },
                 { "bar", 42 },
                 { "baz", new object() }
             };
+            int value = obj.bar;
+            Console.WriteLine(value);
 
 
-
-            //dynamic foo = new ExpandoObject().Init(
-            //    "A".WithValue(true),
-            //    "B".WithValue("Bar"));
-
-
-            //dynamic foo = new ExpandoObject().Init(
-            //    "A".WithValue(true),
-            //    "B".WithValue("Bar"));
-
-
-        }
-            
-        
-
-    }
-    //public static class ExpandoObject
-    //{
-    //    public static KeyValuePair<string, object> WithValue(this string key, object value)
-    //    {
-    //        return new KeyValuePair<string, object>(key, value);
-    //    }
-    //}
-
-    //public static class ExpandoObject
-    //{
-    //    public static ExpandoObject Init(
-    //        this ExpandoObject expando, params KeyValuePair<string, object>[] values)
-    //    {
-            
-    //        foreach (KeyValuePair<string, object> kvp in values)
-    //        {
-    //            ((IDictionary<string, Object>)expando)[kvp.Key] = kvp.Value;
-    //        }
-    //        return expando;
-    //    }
-    //}
-
-    public static class ExtensionMethods
-    {
-        public static ExpandoObject Init(this ExpandoObject expando, dynamic obj)
-        {
-            var expandoDic = (IDictionary<string, object>)expando;
-            foreach (System.Reflection.PropertyInfo fi in obj.GetType().GetProperties())
-            {
-                expandoDic[fi.Name] = fi.GetValue(obj, null);
-            }
-            return expando;
-        }
-    }
+        }   
+    }   
 }
