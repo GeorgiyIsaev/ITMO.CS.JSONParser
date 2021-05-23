a8a5300f3b38351e63d5a8f64a06ce0ada8e15bb
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-23 20:09:26 +0300

    замена MyJsonParser на MyJsonParser02 AND  rename MyJsonParser02 in MyJsonParser

--- ITMO.JSON.Test03.MyParser/MyJsonParser.cs
+++ ITMO.JSON.Test03.MyParser/MyJsonParser.cs
@@ -8,19 +8,23 @@ using System.IO;
 namespace ITMO.JSON.MyParser
 {
     public static class MyJsonParser
-    {      
+    {
+        private static string fulltext;
+        private static List<string> elementGlobal;
+
+
         public static List<dynamic> JsonParser(string namefile)
-        {      
-            string fulltext = ReadFile(namefile);
-            fulltext = DeleteSpace(fulltext);
-            List<string> elementGlobal = Parse(fulltext);
+        {
+            fulltext = ReadFile(namefile);
+            DeleteSpace();
+            elementGlobal = Parse();
 
             Dictionary<string, object> dictionary = new Dictionary<string, object>();
             List<dynamic> listDynamic = new List<dynamic>();
-          
+
             while (elementGlobal.Count > 0)
-            {              
-                KeyValuePair<string, object> element = ElementPara(elementGlobal);                
+            {
+                KeyValuePair<string, object> element = ElementPara();
                 dictionary.Add(element.Key, element.Value);
                 if ("}]" == elementGlobal[0])
                 {
@@ -29,126 +33,112 @@ namespace ITMO.JSON.MyParser
                     {
                         dynamicObj.Add(valueDictionary.Key, valueDictionary.Value);
                     }
-                    listDynamic.Add(dynamicObj);                   
+                    listDynamic.Add(dynamicObj);
                     dictionary = new Dictionary<string, object>();
-                }              
+                }
                 elementGlobal.RemoveAt(0);
-            }                       
+            }
             return listDynamic;
         }
-        private static KeyValuePair<string, object> ElementPara(List<string> elementGlobal)
+
+        private static KeyValuePair<string, object> ElementPara()
         {
             string temp = "";
             Object val;
             KeyValuePair<string, object> myPair;
 
-
             temp = elementGlobal[0].Remove(0, elementGlobal[0].IndexOf("\"") + 1);
             string key = temp.Substring(0, temp.IndexOf("\""));
             temp = temp.Remove(0, temp.IndexOf(":") + 2);
+
             if (temp.Contains('['))
-            {
-                elementGlobal[0] = temp;
-                List<dynamic> listDynamics = new List<dynamic>();
-                while ("}]" != elementGlobal[0])
-                {
-                    Dictionary<string, object> dictionaryInner = new Dictionary<string, object>();
-                    while ("}" != elementGlobal[0])
-                    {
-                        KeyValuePair<string, object> element = ElementPara(elementGlobal);
-                        dictionaryInner.Add(element.Key, element.Value);
-                        if ("}]" == elementGlobal[0])
-                        {
-                            break;
-                        }
-                        else if ("}" == elementGlobal[0])
-                        {
-                            elementGlobal.RemoveAt(0);
-                            break;
-                        }
-                        elementGlobal.RemoveAt(0);
-                    }
-                    dynamic dynamicObj = new Expando();
-                    foreach (var valueDictionary in dictionaryInner)
-                    {
-                        dynamicObj.Add(valueDictionary.Key, valueDictionary.Value);
-                    }
-                    listDynamics.Add(dynamicObj);
-                    if ("}]" == elementGlobal[0])
-                    {
-                        break;
-                    }
-                }
-                val = listDynamics;
+            {                
+                val = ToListType(temp);
                 myPair = new KeyValuePair<string, object>(key, val);
                 return myPair;
             }
             else if (temp.Contains("{"))
             {
-                elementGlobal[0] = temp;
-
-                Dictionary<string, object> dictionaryInner = new Dictionary<string, object>();
-                while ("}" != elementGlobal[0])
-                {
-                    KeyValuePair<string, object> element = ElementPara(elementGlobal);
-                    dictionaryInner.Add(element.Key, element.Value);                  
-                    if ("}" == elementGlobal[0])
-                    {                       
-                        break;
-                    }
-                    elementGlobal.RemoveAt(0);
-                }
-                dynamic dynamicObj = new Expando();
-                foreach (var valueDictionary in dictionaryInner)
-                {
-                    dynamicObj.Add(valueDictionary.Key, valueDictionary.Value);
-                }
+                dynamic dynamicObj =  ToPersonalType(temp); 
                 myPair = new KeyValuePair<string, object>(key, dynamicObj);
                 return myPair;
             }
-            else if (temp.Contains("}]"))
+            else if (temp.Contains("}]") || temp.Contains("} ]"))
             {
                 temp = temp.Substring(0, temp.IndexOf("}"));
                 elementGlobal[0] = "}]";
-            }
-            else if (temp.Contains("} ]"))
-            {
-                temp = temp.Substring(0, temp.IndexOf("}"));
-                elementGlobal[0] = "}]";
-            }
+            }         
             else if (temp.Contains('}'))
             {
                 temp = temp.Substring(0, temp.IndexOf("}"));
                 elementGlobal[0] = "}";
             }
-            if (temp.Contains('\"'))
-            {
-                val = temp.Substring(1, temp.LastIndexOf("\"") - 1); ;
-            }
-            else if (temp == "true") val = true;
-            else if (temp == "false") val = false;            
-            else
-            {
-                val = (object)temp;
-            }
+
+            val = ToObjectSTR(temp);
             myPair = new KeyValuePair<string, object>(key, val);
             return myPair;
         }
 
-
-
-
-        public static Expando JsonParserElement(string elementGlobal)
-        {                    
-          
-            dynamic obj = new Expando()
+        private static Object ToListType(string str)
+        {
+            elementGlobal[0] = str;
+            List<dynamic> listDynamics = new List<dynamic>();
+            while ("}]" != elementGlobal[0])
             {
-                { "foo", "hello" },
-                { "bar", 42 },
-                { "baz", new object() }
-            };        
-            return obj;
+                dynamic dynamicObj = ToPersonalType(str);                             
+                listDynamics.Add(dynamicObj);
+                if ("}]" == elementGlobal[0])
+                {
+                    break;
+                }
+                elementGlobal.RemoveAt(0);
+            }                    
+            return listDynamics;
         }
+
+        private static Object ToPersonalType(string str)
+        {           
+            elementGlobal[0] = str;
+            Dictionary<string, object> dictionaryInner = new Dictionary<string, object>();
+            while ("}" != elementGlobal[0])
+            {
+                KeyValuePair<string, object> element = ElementPara();
+                dictionaryInner.Add(element.Key, element.Value);
+                if ("}" == elementGlobal[0] || "}]" == elementGlobal[0])
+                {
+                    break;
+                }
+                elementGlobal.RemoveAt(0);
+            }
+            dynamic dynamicObj = new Expando();
+            foreach (var valueDictionary in dictionaryInner)
+            {
+                dynamicObj.Add(valueDictionary.Key, valueDictionary.Value);
+            } 
+            return dynamicObj;
+        }
+
+        private static Object ToObjectSTR(string str)
+        {
+            Object val;
+            val = str;
+            int resInt;
+            double resDouble;           
+
+            if (str.Contains('\"'))
+            {
+                val = str.Substring(1, str.LastIndexOf("\"") - 1); ;
+            }
+            else if (str == "true") val = true;
+            else if (str == "false") val = false;
+            else if (Int32.TryParse(str, out resInt)) val = resInt;
+            else if (Double.TryParse(str, out resDouble)) val = resDouble;
+            else
+            {
+                val = (object)str;
+            }
+            return val;
+        }   
 
 
         private static string ReadFile(string namefile)
@@ -162,18 +152,34 @@ namespace ITMO.JSON.MyParser
             return fulltext;
         }
 
-        private static List<string> Parse(string fulltext)
-        {            
-            string[] separator = { ","};
+        private static List<string> Parse()
+        {
+            string[] separator = { "," };
             string[] elementGlobal = fulltext.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-          
+
             List<string> list = new List<string>();
+            string tempAdd="";
             foreach (var i in elementGlobal)
-                list.Add(i);
+            {
+                //if (i[i.Length - 1] != '}')
+                //{
+                //    tempAdd += i;
+                    list.Add(i);
+                //    tempAdd = "";
+                //}
+                //else
+                //{
+                //    tempAdd += i;
+                //}
+                
+                
+            }
+               
 
             return list;
         }
-        private static string DeleteSpace(string fulltext)
+
+        private static string DeleteSpace()
         {
             fulltext = fulltext.Replace("\n", "");
             fulltext = fulltext.Replace("\r", "");
